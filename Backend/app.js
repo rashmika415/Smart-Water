be88cee182f18f1d38fb935376a196a1c428e6c9
rashmika415 <rashmikaprabodana415@gmail.com>
2026-02-17 13:02:23 +0530

    water-usage(error part add)

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const savingPlanRoutes = require("./routes/SavingPlanRoute");
+const usageRoutes = require("./routes/usageRoute");
 
 
 const PORT = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use("/SavingPlan", savingPlanRoutes); // Use saving plan routes
+app.use("/usage", usageRoutes); // Use usage routes
 // CORS middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
